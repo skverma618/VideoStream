92aed23e35f9292f95cce5d2fd6bc22acfd3c9d0
skverma618 <sushilkumarskp618@gmail.com>
2024-07-26 00:36:43 +0530

    minor changes in Admin.ts

--- api/src/controllers/Admin.ts
+++ api/src/controllers/Admin.ts
@@ -50,54 +50,48 @@ export const upload = async (req: any, res: any) => {
 }
 
 const storelink = (videoLink: any) => {
-
-    const newLinkLine = `${videoLink}\n`
+    const newLinkLine = `${videoLink}\n`;
     const linkFilePath = './videoLinks.txt';
     fs.appendFileSync(linkFilePath, newLinkLine, 'utf-8');
-    console.log('[INFO] video URL stored successfully.')
-}
+    console.log('[INFO] video URL stored successfully.');
+};
 
 export const uploadVideo = async (req: any, res: any) => {
     console.log("[INFO] File uploaded.");
 
     const videoId = uuidv4();
-    const videoPath = req.file.path;
-    const outputPath = `./videos/${videoId}`
-    const hlsPath = `${outputPath}/playlist.m3u8` // HLS is a unstiched video file and index.m3u8 work as index for video chunks
+    const videoPath = path.resolve(req.file.path); // Ensure absolute path
+    const outputPath = path.resolve(`./src/videos/${videoId}`);
+    const hlsPath = `${outputPath}/playlist.m3u8`;
 
-    if(!fs.existsSync(outputPath)){
-        fs.mkdirSync(outputPath, {recursive: true})
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, { recursive: true });
     }
-    
-    // Command ref --- https://medium.com/@peer5/creating-a-production-ready-multi-bitrate-hls-vod-stream-dff1e2f1612c
 
-    const ffmpegCommand = `ffmpeg -hide_banner -y -i ${videoPath} \
-    -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename ${outputPath}/360p_%03d.ts ${outputPath}/360p.m3u8 \
-    -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename ${outputPath}/480p_%03d.ts ${outputPath}/480p.m3u8 \
-    -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename ${outputPath}/720p_%03d.ts ${outputPath}/720p.m3u8 \
-    -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename ${outputPath}/1080p_%03d.ts ${outputPath}/1080p.m3u8`
+    const ffmpegCommand = `ffmpeg -hide_banner -y -i "${videoPath}" \
+      -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename ${outputPath}/360p_%03d.ts ${outputPath}/360p.m3u8 \
+      -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename ${outputPath}/480p_%03d.ts ${outputPath}/480p.m3u8 \
+      -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename ${outputPath}/720p_%03d.ts ${outputPath}/720p.m3u8 \
+      -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename ${outputPath}/1080p_%03d.ts ${outputPath}/1080p.m3u8`;
 
-    // This is converter code and can be design in distributed way
-    exec(ffmpegCommand, (error, stdout, stderr) =>{
-        if(error){
-            console.error(`[ERROR] exec error: ${error}`)
-            res.json({"error": "Error while processing your file. Please try again."});
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`[ERROR] exec error: ${error}`);
+            return res.json({ "error": "Error while processing your file. Please try again." });
         }
 
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
 
-        fs.copyFileSync('./playlist.m3u8', `${outputPath}/playlist.m3u8`);
+        const videoUrl = `http://localhost:8000/videos/${videoId}/playlist.m3u8`;
 
-        const videoUrl = `http://localhost:8000/videos/${videoId}/playlist.m3u8`
-
-        try{
+        try {
             storelink(videoUrl);
-        } catch(error){
+        } catch (error) {
             console.error(`[ERROR] error while storing video URL: ${error}`);
-            res.json({"error": "Error while processing your file. Please try again."})
+            return res.json({ "error": "Error while processing your file. Please try again." });
         }
 
-        res.json({"message": "File uploaded successfully.", videoUrl: videoUrl, videoId: videoId})
-    })
-}
+        res.json({ "message": "File uploaded successfully.", videoUrl: videoUrl, videoId: videoId });
+    });
+};
