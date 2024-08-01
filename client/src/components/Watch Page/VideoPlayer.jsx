b61a85e033b34f752b20a7a3652ee5814b0ba371
skverma618 <sushilkumarskp618@gmail.com>
2024-08-02 02:10:23 +0530

    Removed older UI

--- client/src/components/Watch Page/VideoPlayer.jsx
+++ client/src/components/Watch Page/VideoPlayer.jsx
@@ -123,7 +123,7 @@ export const VideoPlayer = () => {
 
       videoElement.classList.add('vjs-big-play-centered');
       videoRef.current.appendChild(videoElement);
-
+      
       const player = playerRef.current = videojs(videoElement, options, () => {
         videojs.log('player is ready');
         onReady && onReady(player);
